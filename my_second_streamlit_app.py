ab0aaa3f75d8fbcd7c408c0ae63bf70c64e204bb
solz9 <125681221+solz9@users.noreply.github.com>
2023-03-17 21:17:40 +0700

    Update my_second_streamlit_app.py

--- my_second_streamlit_app.py
+++ my_second_streamlit_app.py
@@ -6,5 +6,5 @@ model = pickle.load(open(filename, "rb"))
 st.title('Revenue Prediction')
 a = st.number_input('Input Temperature')
 if st.button('Predict'):
-    y_pred = model.predict(a)
-    st.write(y_pred)
+    model = pickle.load(open('revenue_prediction.pickle', 'rb'))
+    st.write(model.predict(a))
