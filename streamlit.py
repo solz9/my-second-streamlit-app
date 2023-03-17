709635261ceae9dc181bbedc5b5af21e104ed1c3
solz9 <125681221+solz9@users.noreply.github.com>
2023-03-17 21:13:34 +0700

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+df = pd.read_csv('IceCreamData.csv')
+df.head()
+x1 = df['Temperature'].values
+y1 = df['Revenue'].values
+
+x_train, x_test, y_train, y_test = train_test_split(x1, y1, test_size = 50/500) 
+model = LinearRegression()
+x_train = x_train.reshape(-1, 1)
+model.fit(x_train, y_train)
