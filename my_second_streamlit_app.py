6d2fe664fbd4d3299ee363993a899d85c0b3cc7f
solz9 <125681221+solz9@users.noreply.github.com>
2023-03-19 10:28:25 +0700

    Update my_second_streamlit_app.py

--- my_second_streamlit_app.py
+++ my_second_streamlit_app.py
@@ -1,9 +1,6 @@
 import pickle
 import streamlit as st
 
-st.title('Revenue Prediction')
-a = st.number_input('Input Temperature')
+st.title('KẾT QUẢ HỌC TẬP LỚP LÝ 4')
+a = st.number_input('Nhập họ và tên')
 if st.button('Predict'):
-    model = pickle.load(open('model.pickle', 'rb'))
-    r = model.predict([[a]])[0]
-    st.write(r)
